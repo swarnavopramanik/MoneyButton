96795db303f49928589d6dba05b31f4e833de414
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-07-03 17:02:07 +0530

    Add files via upload

---
+++ src/components/PayWall.jsx
@@ -0,0 +1,49 @@
+//paywall component will go here
+import React from 'react';
+import MoneyButton from '@moneybutton/react-money-button'
+
+class PayWall extends React.Component {
+
+    state = {
+        paid: false,
+        lastPayment: null
+    }
+
+    mbOnPaymentCallback = (payment) => {
+        this.setState({paid: true});
+        this.setState({lastPayment: payment});
+        //call the onPayment event handler that was passed to us
+        if (this.props.onPayment) {
+            this.props.onPayment(payment);
+        }
+    }
+
+    render () {
+        //TODO: check if payment expired
+        return (
+            <div>
+                { !this.state.paid ? (
+                <MoneyButton to='145' type='buy' amount='1' currency='USD' 
+                buttonData='{"website":"GitHub","category":"paywall","description":"swipe code","owner":"swarnavo"}'
+                label="Test Your Button"
+                onPayment={this.mbOnPaymentCallback}></MoneyButton>
+                ) : null}
+
+{ !this.state.paid ? (
+                <MoneyButton to='145' type='buy' amount='10' currency='USD' 
+                buttonData='{"website":"GitHub","category":"paywall","description":"swipe code","owner":"swarnavo"}'
+                label="% TIP "
+                onPayment={this.mbOnPaymentCallback}></MoneyButton>
+                ) : null}
+
+
+            </div>
+          
+        );
+
+            
+            
+    }
+}
+
+export default PayWall;
